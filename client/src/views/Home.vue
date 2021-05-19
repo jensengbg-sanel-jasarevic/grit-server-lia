73be7549aef4d6d48baad34efd7c22cab70b1425
Sanel Jasarevic <saneljasar199212@gmail.com>
2021-05-19 15:40:08 +0200

    docker compose

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,74 @@
 <template>
   <div class="home">
-    <HelloWorld />
+
+    <button @click.prevent="postSketchDraft">POST Sketch&Draft</button>
+      <div v-if="postSketchDraftBtn" class="confirmed">
+        <p>Inspect console for results</p> 
+      </div> 
+    <br>
+    <br> 
+
+    <form @submit.prevent="updateSketch">
+      <input type="number" placeholder="Sketch ID to change">
+      <input type="text" placeholder="Changes">
+      <button type="submit">PATCH Sketch</button>
+        <div v-if="patchSketchBtn" class="confirmed">
+          <p>Inspect console for results</p> 
+        </div>    
+    </form>
+    <br>
+    <br>  
+  
+    <form @submit.prevent="deleteDraft">
+      <input type="number" placeholder="Draft ID to delete">
+    <button>DELETE Draft</button>
+      <div v-if="deleteDraftBtn" class="confirmed">
+        <p>Inspect console for results</p> 
+      </div>  
+    </form>
+    <br>
+    <br>
+    
+    <form @submit.prevent="postOrder">
+      <input type="number" placeholder="Draft ID to order">
+    <button>POST Order</button> 
+      <div v-if="postOrderBtn" class="confirmed">
+        <p>Inspect console for results</p> 
+      </div>
+    </form>
+  
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data: () => ({
+      postSketchDraftBtn: false,
+      patchSketchBtn: false,
+      deleteDraftBtn: false,
+      postOrderBtn: false
+  }),
+
+  methods: {
+    postSketchDraft(){
+    this.$store.dispatch('postSketchDraft') 
+    this.postSketchDraftBtn = true
+    },
+    updateSketch(e){
+    this.$store.dispatch('updateSketch', { id: e.target[0].valueAsNumber, changes: e.target[1].value })
+    this.patchSketchBtn = true
+    },
+    deleteDraft(e){
+    this.$store.dispatch('deleteDraft', e.target[0].valueAsNumber)
+    this.deleteDraftBtn = true
+    },
+    postOrder(e){
+    this.$store.dispatch('postOrder', e.target[0].valueAsNumber),
+    this.postOrderBtn = true
+    },
+
   }
+  
 }
 </script>
