76096d8f65363767d03db54cd7f2fbcc494ab907
Sanel Jasarevic <saneljasar199212@gmail.com>
2021-05-19 10:55:59 +0200

    tables for clients & contacts

--- migrations/20210502013341_main.js
+++ migrations/20210502013341_main.js
@@ -1,20 +1,34 @@
 exports.up = function(knex) {
     return knex.schema
+
+    .createTable("clients", tbl => { 
+      tbl.increments()
+      tbl.text("name", 128).notNullable();
+    })
+
+    .createTable("contacts", tbl => { 
+      tbl.increments()
+      tbl.text("name", 128).notNullable();
+    })
+
     .createTable("sketches", tbl => { // Callback function to modify the table's structure, using the schema-building commands
       tbl.increments('id')
       tbl.text("message")
+      tbl.text("changes")
       tbl.timestamps(true, true) 
     })
 
     .createTable("drafts", tbl => { 
-      tbl.increments(),
+      tbl.increments()
       tbl.text("message")
+      tbl.text("changes")
       tbl.timestamps(true, true);
     })
         
     .createTable("orders", tbl => { 
       tbl.increments()
       tbl.text("message")
+      tbl.text("changes")
       tbl.timestamps(true, true);
     })
   }
