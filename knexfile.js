730b4a7e71ded89ba15e3a50d5e2054ba81f0520
Sanel Jasarevic <saneljasar199212@gmail.com>
2021-08-17 13:28:08 +0200

    knexfile modifications

--- knexfile.js
+++ knexfile.js
@@ -4,24 +4,27 @@ module.exports = {
       client: 'sqlite3', 
       useNullAsDefault: true, // Gets rid of bunch of bugs just for sqlite.
       connection: {
-        // Our database. Type what name file should have when running -> knex migrate:latest
-        filename: "./data/main.db3",
+        // We first define a database connection and a migrations directory.
+        // Generate a migration file invoking the 'knex migrate:make'. Schema builder will be in this file. All tables should be defined there.
+        // Run 'knex migrate:latest' to drop our schema builder with tables (database) to directory defined below. 
+        filename: "./data/main.db3", // SQLite can have extension '.db3' or '.sqlite3'. 
+        // This is now the file with the database.
       },
       pool: {
-        // Enforce foreign keys relationship between tables
+        // Enforce foreign keys relationship between tables.
         afterCreate: (conn, done) => {
           conn.run("PRAGMA foreign_keys = ON", done);
         },
       },
     },
     production: {
-      client: "pg", // Determine which database being used
+      client: "pg", // Determine which database management system should be used.
       connection: { 
         connectionString: process.env.DATABASE_URL,
         ssl: { rejectUnauthorized: false }
       }
     },  
-      // Required according to knex documentation 
+      // Required by Knex documentation. 
       pool: {
         min: 2,
         max: 10,
--- migrations/20210502013341_main.js
+++ migrations/20210502013341_main.js
@@ -1,7 +1,16 @@
+// The up function is where we write code that updates the database to the next version.
+// The down function is where we write code that restores the previous version of the database.
+
+// Run 'npx knex migrate:latest' to create migration that will populate 'data' folder.  
+
 exports.up = function(knex) {
+  // Database schema
     return knex.schema
-    .createTable("sketches", tbl => { // Callback function to modify the table's structure, using the schema-building commands
-      tbl.increments()
+    // We pass the table name as the first argument and a callback function as the second argument..
+    .createTable("sketches", tbl => { // Callback function to modify table's structure using the schema-building commands/methods.
+      // To create columns we call methods on the tbl object. Depending on the column we want to create, we call different methods.
+      // One column that most tables have is a primary key column. This column is used to keep the record id, and it usually contains an auto incremented integer.
+      tbl.increments() // Here's how we create a primary key column named 'id' that will be incremented each time a record is created:
       tbl.text("message")
       tbl.text("changes")
       tbl.timestamps(true, true) 
@@ -34,8 +43,7 @@ exports.up = function(knex) {
     })
   }
   
+  // Reverse our migrations, in the down function of our migration file we add code that removes the whole table we created in the up function.
   exports.down = function(knex) {
       return knex.schema.dropTableIfExists("sketches")
   };
-  
-  // Create a migration that will populate data folder, npx knex migrate:latest 
