51f24b8962cff6f811a7d40a3541484eb5da6628
Sanel Jasarevic <saneljasar199212@gmail.com>
2021-08-18 10:01:00 +0200

    home route

--- Routes/home-routes.js
+++ Routes/home-routes.js
@@ -1,11 +1,33 @@
 const express = require ("express");
 const dbQueries = require("../model/database-queries")
+const shortid = require('shortid');
 
 const router = express.Router() 
 
-// GET sketches
+// GET home page
 router.get("/", (req, res) => {
-    res.json( { message: "Homeeee"} )
+    res.json( { message: "Welcome"} )
 })
+
+// POST record to sketches & drafts table
+router.post("/", async (req, res) => {
+    const sketchID = shortid.generate()
+
+    dbQueries.addSketch({ message: `Sketch record (${sketchID}) added for sketch table` })
+    .then(response => { 
+    console.log(response)
+    })
+    .catch(error => { 
+        console.error(error);
+    })
+
+    dbQueries.addSketchToDrafts({ message: `Sketch (${sketchID}) record added to draft table` })
+     .then(response => { 
+         console.log(response)
+         res.status(200).json(response) 
+    })
+     .catch(error => { 
+         res.status(500).json({ message: "Unable to perform operation" }) });
+ })
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const shortid = require('shortid');
 const cors = require('cors')
 
-const dbQueries = require("../model/database-queries")
-
+// Path to directories 
 const homeRouter = require("../Routes/home-routes")
 const sketchesRouter = require("../Routes/sketch-routes")
 const draftsRouter = require("../Routes/draft-routes")
@@ -14,32 +12,8 @@ const awsRouter = require("../Routes/aws-routes")
 const server = express() 
 server.use(express.json()) 
 server.use(cors())
-/*
-server.get("/", (req, res) => {
-    res.json( { message: "Home"} )
-});
-*/
-// POST record to sketches & drafts table
-server.post("/", async (req, res) => {
-    const sketchID = shortid.generate()
 
-    dbQueries.addSketch({ message: `Sketch record (${sketchID}) added for sketch table` })
-    .then(response => { 
-    console.log(response)
-    })
-    .catch(error => { 
-        console.error(error);
-    })
-
-    dbQueries.addSketchToDrafts({ message: `Sketch (${sketchID}) record added to draft table` })
-     .then(response => { 
-         console.log(response)
-         res.status(200).json(response) 
-    })
-     .catch(error => { 
-         res.status(500).json({ message: "Unable to perform operation" }) });
- })
-
+// API Endpoints
 server.use("/", homeRouter)
 server.use("/api/sketches", sketchesRouter)
 server.use("/api/drafts", draftsRouter)
